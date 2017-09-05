0685db913600afc322a2daefa615d2b9bbdcd7de
ignaszienius <30698624+ignaszienius@users.noreply.github.com>
2017-09-05 16:55:08 +0300

    Padariau dizainą ND

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Temperatūra ir pojūtis</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+</head>
+<body>
+<div class="container-fluid">
+	<div class="row head bg-warning">
+		<h1>Temperatūra ir pojūtis</h1>
+	</div>
+	<div class="row">
+		<div class="col-6">
+			<table class="table table-inverse">
+				  <thead>
+				    <tr>
+				      <th>Temperatūra</th>
+				      <th>Pojūtis</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <td>Mark</td>
+				      <td>Otto</td>
+				    </tr>
+				    <tr>
+				      <td>Jacob</td>
+				      <td>Thornton</td>
+				    </tr>
+				    <tr>
+				      <td>Larry</td>
+				      <td>the Bird</td>
+				    </tr>
+				  </tbody>
+			</table>
+		</div>	
+		<div class="col-6">
+		<ul class="list-group">
+		  <li class="list-group-item">Temperatūros pateikiamos kas 3C intervale -30 ... +35</li>
+		  <li class="list-group-item">Jeigu temperatūra mažesnė arba lygi -20 - labai šalta</li>
+		  <li class="list-group-item">Jeigu temperatūra mažesnė arba lygi +5 - šalta</li>
+		  <li class="list-group-item">Jeigu temperatūra mažesnė arba lygi +20 - šilta</li>
+		  <li class="list-group-item">Jeigu temperatūra mažesnė arba lygi +25 - karšta</li>
+		  <li class="list-group-item">Jeigu temperatūra didesnė +25 - labai karšta</li>
+		</ul>
+		</div>
+		
+	</div>
+
+</body>
+</html>
